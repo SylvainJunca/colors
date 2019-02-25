ff1465a9cac27a51277abafa10f7ce3cc3858adb
SylvainJunca <sylvain.junca@gmail.com>
2019-02-24 23:57:28 -0500

    validation of fields almost done

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ class App extends Component {
         phone: '',
         color: ''
       },
-      errors: {}
+      component: 'color';
+      errors: {},
+      formValid: false,
     }
   }
 
@@ -27,28 +29,47 @@ class App extends Component {
   validateForm = () => {
     const fields = this.state.fields;
     const errors = {};
+    let formValid = true;
     
     const emailPattern = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
     // regular expression found there https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
     if(!emailPattern.test(fields['email'])) {
       errors['email'] = 'Email should be a valid email';
+      formValid = false;
     }
     if (!fields['phone'].match(/^[0-9]{10}$/)) {
       errors['phone'] = 'Phone number should have the format +1 XXX XXX XX XX (Where X is a digit)';
+      formValid = false;
     } 
+
+    if(fields['color'].match(/^[A-Z]*$/)) {
+      if(fields['color'] !== 'BLACK' &&
+         fields['color'] !== 'BLUE' &&
+         fields['color'] !== 'RED' &&
+         fields['color'] !== 'GREEN') {
+           errors['color'] = 'Allowed colors are BLACK, BLUE, RED, GREEN';
+           formValid = false;
+         }
+    } else {
+      errors['color'] = 'Color must be alphabet only and uppercase';
+      formValid = false;
+    }
 
     for (const field in fields) {
       if(fields[field] === '') {
         errors[field] = 'This is a required field';
+        formValid = false;
       }
     }
-
-    this.setState({ errors });
+    formValid ? this.setState({formValid : true}) : this.setState({ errors });
   }
 
   submitForm = event => {
     event.preventDefault();
     this.validateForm();
+    if(this.state.formValid){
+      alert('yeah');
+    }
   }
 
   render() {
